b633a0b87329080b75877fc12dd626a7c8c2b1fb
Mark Virag <mark@viragconsulting.com>
2024-08-23 18:41:11 -0500

    Added text box to faciliate examining the context data

--- query_data.py
+++ query_data.py
@@ -6,15 +6,30 @@ from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain_chroma import Chroma 
 import chromadb
+import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
 
 CHROMA_PATH = "chroma"
 
-PROMPT_TEMPLATE = """Answer this question {question} including the following context: {context}"""
+PROMPT_TEMPLATE = """Answer this question: {question} based on these excerpts: {context}"""
+
+def show_text(title, text):
+    window = tk.Tk()
+    window.title(title)
+    window.geometry("650x300") 
+    text_widget = tk.Text(window, wrap="word", font=("Arial", 10))
+    text_widget.pack(side="left", fill="both", expand=True)
+    scrollbar = tk.Scrollbar(window, orient="vertical", command=text_widget.yview)
+    scrollbar.pack(side="right", fill="y")
+    text_widget.config(yscrollcommand=scrollbar.set)
+    text_widget.insert(tk.END, text)
+    window.mainloop()
 
 def main():
     embedding_function = OpenAIEmbeddings()
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     model = ChatOpenAI()
+ 
     query_text = input("Enter query: ")
     while (query_text != "quit"):   
 
@@ -25,13 +40,13 @@ def main():
         if len(results) == 0 or results[0][1] < 0.7:
             print(f"Unable to find matching results.")
         else:
-            context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+            context_text = "\n---\n".join([doc.page_content for doc, _score in results])
             prompt = prompt_template.format(context=context_text, question=query_text)      
-            response = model.invoke(prompt)
-            sources = [doc.metadata.get("source", None) for doc, _score in results]
-            print("\nCONTEXT: " + context_text + "\n")
-            print("\nRESPONSE: " + response.content + "\n")
-            # print(sources)
+            # sources = [doc.metadata.get("source", None) for doc, _score in results]
+            source_listing = [doc.metadata.get("source", None) + "  {:4.2f}".format(_score) + "\n" + doc.page_content + "\n\n" for doc, _score in results]
+            # response = model.invoke(prompt)
+            # print("\n" + response.content + "\n")
+            show_text("Source Data", source_listing)
 
         query_text = input("Enter query: ")
 
