577f12eaae8c8c84470d41b9eb334eb6a0db13f5
Mark Virag <mark@viragconsulting.com>
2024-08-26 18:16:00 -0500

    Went back to LangChain (faster) and handled Eliot

--- read_database.py
+++ read_database.py
@@ -6,10 +6,14 @@ import chromadb
 from chromadb.config import Settings
 
 load_dotenv()
-CHROMA_PATH = "chroma" 
-collection_name = "tolstoy"
+collection_name = "eliot"
+DATA_PATH = "data/" + collection_name
+# DATA_PATH = "data/eliot" 
+CHROMA_PATH = DATA_PATH + "/chroma" 
 
 client = chromadb.Client(Settings(is_persistent=True, persist_directory=CHROMA_PATH))
+print(client.list_collections())
+
 collection = client.get_collection(name=collection_name)
 document_id = "1"
 result = collection.get(
@@ -17,3 +21,4 @@ result = collection.get(
     include=["embeddings"], # Embeddings are excluded unless you ask for them 
     )
 for item in result: print(item, result[item])  
+print(len(result["embeddings"][0])) # It looks like the OpenAI default is 1536 
