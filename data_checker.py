933c5331becdb63bc76a1d9c2bc024888b864628
Mark Virag <mark@viragconsulting.com>
2024-09-15 16:49:52 -0500

    Added clustering with UMAP

--- data_checker.py
+++ data_checker.py
@@ -8,9 +8,12 @@ from chroma_utils import CrossDistances, DisplayOne, GetCollection, GetDocById
 from sklearn.decomposition import PCA
 from sklearn.cluster import AgglomerativeClustering
 from scipy.cluster.hierarchy import dendrogram
+import umap
+import hdbscan 
+sns.set_palette(sns.color_palette() )
 
 collection_name = "thackeray"
-collection_path = "chroma3" 
+collection_path = "chroma2" 
 model_name = "text-embedding-3-small"
 
 def plot_dendrogram(model, **kwargs):
@@ -43,42 +46,38 @@ def main():
     count = collection.count()
     print(f"Documents: {count:,}")
 
-    doc = GetDocById(collection, 0, True)
-    dims = len(doc["embeddings"][0])
-    print(f"Dimensions: {dims:,}")
-
     docs = collection.get(include=["embeddings"])
     vectors = docs["embeddings"]
-    ids = docs["ids"]
+    dims = len(vectors[0])
+    print(f"Dimensions: {dims:,}")
     
     rng = np.random.default_rng()
     rand_item = rng.integers(0, count)
-    DisplayOne("Some doc", GetDocById(collection, rand_item))
+    # DisplayOne("Some doc", GetDocById(collection, rand_item))
     
-    n_comps = 60
+    n_comps = 20
     pca = PCA(n_components=n_comps)
     reduced = pca.fit_transform(vectors)
     print(f"Variance explained: {pca.explained_variance_ratio_.sum(): 0.2f} by: {n_comps:,} components")
 
-    fig = plt.figure(figsize=(9,5))
-    ax = fig.add_subplot(111)
+    fig1 = plt.figure(figsize=(11,5))
+    ax = fig1.add_subplot(121)
     ticks = np.arange(pca.n_components_)+1
     ax.plot(ticks,
         pca.explained_variance_ratio_.cumsum(),
         marker='o')
     ax.set_xlabel('Principal Component');
-    ax.set_ylabel('Cumulative Proportion of Variance Explained')
+    ax.set_ylabel('Cumulative Variance')
+    ax.set_title('Proportion of Variance Explained')
     ax.set_ylim([0,1])
     ax.set_xticks(ticks)
-    plt.show()
 
     self_similarity = CrossDistances(collection, collection, stats=False)
     max_dist = 1 - np.min(self_similarity)
     print(f"Max distance: {max_dist: .2f}")
     self_similarity = [x for x in self_similarity if x < 0.99] # Remove the same-doc results 
 
-    fig = plt.figure(figsize=(9,5))
-    ax1 = fig.add_subplot(111)
+    ax1 = fig1.add_subplot(122)
     ax1.hist(self_similarity, bins=200, density=True)
     ax1.set_xlabel('Cosine Similarity')
     ax1.set_ylabel('Frequency')
@@ -86,13 +85,44 @@ def main():
     plt.show()
 
     linkage = 'complete'
-    cluster_model = AgglomerativeClustering(n_clusters=6, metric='cosine', linkage=linkage, compute_distances=True)
+    cluster_model = AgglomerativeClustering(n_clusters=None, metric='cosine', linkage=linkage, compute_distances=True, distance_threshold=0)
     clusters = cluster_model.fit_predict(vectors)
  
     plt.title("Hierarchical Clustering: " + collection_name + ", Linkage: " + linkage)
-    plot_dendrogram(cluster_model, truncate_mode="level", p=5, color_threshold=0.25)
+    plot_dendrogram(cluster_model, truncate_mode="level", p=4, color_threshold=(max_dist*0.75))
     plt.xlabel("Number of points in node (or index of point if no parenthesis).")
     plt.show()
+
+    neighbors = int(count * 0.10)
+    manifold = umap.UMAP(
+        n_neighbors=neighbors,
+        metric='cosine',
+        min_dist=0.0,
+        n_components=3,
+    ).fit_transform(vectors)
+
+    size = int(count * 0.02)
+    hdb_model = hdbscan.HDBSCAN(
+        min_cluster_size=size,
+        metric='euclidean' # Manifold is simple 3D 
+    ).fit(manifold)
+    labels = hdb_model.labels_
+    print(np.max(labels)) # Values < 0 are noise 
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(
+        manifold[:, 0],
+        manifold[:, 1],
+        manifold[:, 2],
+        c=labels,
+        s=7,
+    )
+    plt.gca().set_aspect('equal', 'datalim')
+    plt.title("UMAP projection of " + collection_name)
+    plt.show()
+
+
 
 if __name__ == "__main__":
     main()
