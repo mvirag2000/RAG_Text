6512b427da193238d2db52e03c86c4aba89ee7c9
Mark Virag <mark@viragconsulting.com>
2026-09-06 16:53:12 -0500

    Enhanced old word2vec script, added new PCA_words

---
+++ PCA_words.py
@@ -0,0 +1,96 @@
+##
+## Pretty much the same routines as word2vec.py only with a better embedding
+##
+from openai import OpenAI
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D 
+import matplotlib.animation as animation
+from sklearn.decomposition import PCA
+from wordfreq import top_n_list
+
+vocab = top_n_list('en', 50000)
+vocab_index = {word: i for i, word in enumerate(vocab)} 
+client = OpenAI()   
+
+def load_vectors(init=True, model="text-embedding-3-small", batch_size=2048):
+    # Refresh matrix only as needed 
+    if (init):
+        all_vectors = []
+        for i in range(0, len(vocab), batch_size):
+            chunk = vocab[i:i + batch_size]
+            resp = client.embeddings.create(model=model, input=chunk)
+            all_vectors.extend([d.embedding for d in resp.data])
+        matrix = np.array(all_vectors)
+        np.save("vocab_embeddings.npy", matrix)
+    else:
+        matrix = np.load("vocab_embeddings.npy")
+    return matrix
+
+def vector(word): # Fresh call to OpenAPI - guaranteed to find a valur
+    response = client.embeddings.create(model="text-embedding-3-small", input=word)
+    return response.data[0].embedding
+
+def get_vectors(words): # Lookup in matrix - limited vocabulary 
+    missing = [w for w in words if w not in vocab_index]
+    if missing:
+        raise ValueError(f"Not in vocab: {missing}")
+    return matrix[[vocab_index[w] for w in words]]
+
+def similarity(a, b):
+    a, b = np.array(a), np.array(b)
+    return a.dot(b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+def distance():
+    a = input("Input first word: ")
+    b = input("Input second word: ")
+    simil = similarity(vector(a), vector(b))
+    print(simil)
+
+def similar_words(tol):
+    w= input("Input word: ")
+    target = vector(w)
+    sims = matrix @ target   
+    mask = sims >= tol
+    results = sorted(zip(np.array(vocab)[mask], sims[mask]), key=lambda x: x[1], reverse=True)
+    for x in results:
+        print(x[0], round(x[1], 5))
+    plt.hist(sims, bins=100)
+    plt.title('Distribution of words near \"' + w + '\"')
+    plt.xlabel('Similarity')
+    plt.ylabel('Words')
+    plt.axvline(x=0, color='red', linestyle='--', linewidth=1)
+    plt.show()
+
+def project3D(points, labels):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    three_D = points
+    x, y, z = three_D[:,0], three_D[:,1], three_D[:,2]
+    ax.scatter(x, y, z)
+    ax.set_xlabel('X Axis')
+    ax.set_ylabel('Y Axis')
+    ax.set_zlabel('Z Axis')
+    for label, x, y, z in zip(labels, x, y, z): 
+        ax.text(x, y, z, label)
+        print(label, x, y, z)
+    def rotate(angle): 
+        ax.view_init(elev=10, azim=angle)
+    rot_animation = animation.FuncAnimation(fig, rotate, frames=np.arange(0, 360, 2))
+    plt.show()
+    rot_animation.save('EmbeddingPCA.gif')
+
+matrix = load_vectors(init=False) # Flag to reinitialize matrix 
+# similar_words(0.40)
+# distance()
+value = ['diamond', 'bullion', 'money', 'wealth', 'rand', 'coins', 'bucks']
+color = ['yellow', 'grey', 'rose', 'green', 'orange', 'pink', 'blue'] # All within 0.45 of "gold " 
+metal = ['iron', 'metal', 'bronze', 'silver', 'chrome', 'palladium']
+some_words = ['gold'] + value + color + metal 
+print(some_words)
+some_vectors = get_vectors(some_words)
+pca = PCA(n_components=3)
+reduced = pca.fit_transform(some_vectors)
+print("Unexplained: %1.2f" % (100 - 100*np.sum(pca.explained_variance_ratio_)))
+
+project3D(reduced, some_words)
--- word2vec.py
+++ word2vec.py
@@ -4,9 +4,11 @@
 ##
 import gensim.downloader as api
 from gensim.models import KeyedVectors
+from scipy.stats import differential_entropy
 from sklearn.preprocessing import normalize
 import re
 import numpy as np
+import matplotlib.pyplot as plt
 
 def load_model(init=True):
     # Refresh monster file only as needed 
@@ -30,37 +32,57 @@ def load_model(init=True):
         clean_model = KeyedVectors.load("cleaned-model")
     return clean_model
 
-def in_tolerance(num1, num2, tol=0.00001):
+def in_tolerance(num1, num2, tol=0.0001):
     if ((num1 - num2) > tol): return False
     if ((num2 - num1) > tol): return False
     return True
 
-#giant_score = input("Giant score: ")
-#enemy_score = input("Enemy score: ")
-model = load_model(False)
+def solver():
+    giant_score = input("Giant score: ")
+    enemy_score = input("Enemy score: ")
+    giant = float(giant_score) / 100
+    enemy = float(enemy_score) / 100
+    tolerance = 0.0001
+    for w in keys:
+        simil_giant = model.similarity(w, "giant")
+        if in_tolerance(simil_giant, giant):
+            simil_enemy = model.similarity(w, "enemy")
+            if in_tolerance(simil_enemy, enemy):
+                print(w)
+
+def similar_words(tol):
+    w = input("Input word: ")
+    simil_list = []
+    for word in words:
+        simil = model.similarity(word, w)
+        if (simil > tol):
+            print(f"{word} {simil:,.6f}")
+        simil_list.append(simil)
+    plt.hist(simil_list, bins=100)
+    plt.title('Distribution of words near \"' + w + '\"')
+    plt.xlabel('Similarity')
+    plt.ylabel('Words')
+    plt.axvline(x=0, color='red', linestyle='--', linewidth=1)
+    plt.show()
+
+def different_words(tol):
+    w = input("Input word: ")
+    for word in words:
+        simil = model.similarity(word, w)
+        if (simil < tol) and (simil > -0.12): # Below -0.15 are junk words
+            print(f"{word} {simil:,.6f}")
+
+def distance():
+    a = input("Input first word: ")
+    b = input("Input second word: ")
+    simil = model.similarity(a, b)
+    print(simil)
+
+model = load_model(False) # Flag to reinitialize model 
 keys = model.key_to_index.keys()
 words = list(model.index_to_key)
 
-# The Marinade Digression
-for word in words:
-    simil_marinade = model.similarity(word, "marinade")
-    if (simil_marinade > 0.70):
-        print(f"{word} {simil_marinade:,.6f}")
-
-stop
-
-# Semantle Solver
-giant = float(giant_score) / 100
-enemy = float(enemy_score) / 100
-tolerance = 0.0001
-for w in keys:
-    simil_giant = model.similarity(w, "giant")
-    if in_tolerance(simil_giant, giant):
-        simil_enemy = model.similarity(w, "enemy")
-        if in_tolerance(simil_enemy, enemy):
-            print(w)
-
-
-
-
-
+# solver()
+similar_words(0.55)
+# distance()
+# different_words(-0.05) 
